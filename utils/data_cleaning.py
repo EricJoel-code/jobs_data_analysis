948273fed585bcdbc3623c6e6a43a06bb7551200
EricJoel-Code <joeld214254@gmail.com>
2026-02-14 22:15:39 -0500

    Add file data_cleaning.py

---
+++ utils/data_cleaning.py
@@ -0,0 +1,16 @@
+import re
+
+class DataCleaning:
+    def __init__(self):
+        pass
+    
+    def clean_data_jobs(self, df):
+        df = df.drop_duplicates(subset=['id'])
+        df = df.dropna(subset=['description'])
+        df = df.drop(df[df.id == "".index])
+        df = df.reset_index()
+        df = df.drop(['index'], axis=1)
+        
+        df['location'] = df['location'].fillna("")
+        
+        return df
