495e84bb48b646795bf6e1c7335bd7de910bca30
EricJoel-Code <joeld214254@gmail.com>
2026-02-18 21:27:51 -0500

    Update main.py and data_cleaning.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import sqlite3
 from utils.scraper import JobScraper
+from utils.data_cleaning import DataCleaning
 
 def main():
-    searches = ["data analyst", "data scientist", "python developer"]
+    '''searches = ["data analyst", "data scientist", "python developer"]
     sites = ["indeed"]
     results = 1000
     old = 1000
@@ -15,7 +16,19 @@ def main():
     con = sqlite3.connect('jobs.db')
     df.to_sql('jobs_raw_table', con, if_exists='append', index=False)
     
-    df.to_csv('jobs_raw_table.csv')
+    df.to_csv('jobs_raw_table.csv')'''
+    
+    conn = sqlite3.connect('jobs.db')
+    query = "SELECT * FROM jobs_raw_table"
+    df = pd.read_sql_query(query, conn)
+    
+    datacleaner = DataCleaning()
+    
+    df_processed = datacleaner.clean_data_jobs(df)
+    
+    df_processed.to_sql('jobs_cleaned_table', conn, if_exists = 'append', index=False)
+    
+    conn.close()
     
 if __name__ == "__main__":
     main()
--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -7,7 +7,7 @@ class DataCleaning:
     def clean_data_jobs(self, df):
         df = df.drop_duplicates(subset=['id'])
         df = df.dropna(subset=['description'])
-        df = df.drop(df[df.id == "".index])
+        df = df.drop(df[df.id == ""].index)
         df = df.reset_index()
         df = df.drop(['index'], axis=1)
         
